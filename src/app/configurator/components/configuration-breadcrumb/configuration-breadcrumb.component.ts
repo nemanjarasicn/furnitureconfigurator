a09dd7f082e7b5cf34a1622ca5a0b577bc530821
Strahinja Belic <belicg22@gmail.com>
2021-02-26 13:30:21 +0100

    scroller links fixed on config page

--- src/app/configurator/components/configuration-breadcrumb/configuration-breadcrumb.component.ts
+++ src/app/configurator/components/configuration-breadcrumb/configuration-breadcrumb.component.ts
@@ -1,26 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ConfigurationConstants } from 'src/app/common/constants/configuration.constants';
 import { IBreadcrumb } from 'src/app/common/models/interfaces/breadcrumb.interface';
-
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-configuration-breadcrumb',
   templateUrl: './configuration-breadcrumb.component.html',
-  styleUrls: ['./configuration-breadcrumb.component.scss']
+  styleUrls: ['./configuration-breadcrumb.component.scss'],
 })
 export class ConfigurationBreadcrumbComponent implements OnInit {
-
-
-
   breadcrumbs: IBreadcrumb[] = ConfigurationConstants.getConfigurationBreadCrumbs();
   activeAnchor: string = this.breadcrumbs[0].anchor;
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: any) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onBreadcrumbClick(breadcrumb: IBreadcrumb) {
     this.activeAnchor = breadcrumb.anchor;
+    this.document.querySelector(this.activeAnchor).scrollIntoView();
   }
-
 }
